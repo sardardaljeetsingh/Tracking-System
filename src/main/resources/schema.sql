53cf3353477d79ce260c94c3555cd6298af49eb4
350401 <pavan.rankireddy@gmail.com>
2017-05-18 03:54:09 +0530

    dfdfd

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,4 +17,4 @@ CREATE TABLE company
 CREATE TABLE group_dtl( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR2(50) NOT NULL, companyid INT NOT NULL, parentid INT NOT NULL, );
 
 ALTER TABLE group_dtl ADD FOREIGN KEY ( companyid ) REFERENCES company( id ) ;
-ALTER TABLE group_dtl ADD FOREIGN KEY ( parent ) REFERENCES group_dtl( id ) ;
+ALTER TABLE group_dtl ADD FOREIGN KEY ( parentid ) REFERENCES group_dtl( id ) ;
