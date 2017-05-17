2759f0be9869fd4018b5dc635070bce8d9502335
350401 <pavan.rankireddy@gmail.com>
2017-05-18 03:24:02 +0530

    data added to Data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,7 @@ INSERT INTO MODEL (ID, VAL) VALUES (4, 'Karunakar');
 INSERT INTO MODEL (ID, VAL) VALUES (5, 'TEST');
 
 INSERT INTO company (id ,name,address,city,tin,phonenumber) VALUES (1, 'IBM','MIndSPace','HYD',1234,99999);
+
+INSERT INTO group (id ,name,companyid,parent) VALUES (1, 'Raymond',1,0);
+INSERT INTO group (id ,name,companyid,parent) VALUES (2, 'Shuttings',1,1);
+
