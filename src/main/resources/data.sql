1b0ac274ed3ed45ea3dec06fed04c6513eb0a596
350401 <pavan.rankireddy@gmail.com>
2017-05-18 02:30:13 +0530

    records added to company Table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,5 @@ INSERT INTO MODEL (ID, VAL) VALUES (2, 'Daljeet');
 INSERT INTO MODEL (ID, VAL) VALUES (3, 'Santosh');
 INSERT INTO MODEL (ID, VAL) VALUES (4, 'Karunakar');
 INSERT INTO MODEL (ID, VAL) VALUES (5, 'TEST');
+
+INSERT INTO company (id ,name,address,city,tin,phonenumber) VALUES (1, 'IBM','MIndSPace','HYD',1234,99999);
