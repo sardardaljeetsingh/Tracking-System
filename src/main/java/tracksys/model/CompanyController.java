029bf61b1e7c7476aafb9b0d668a488ab1a5f2f6
350401 <pavan.rankireddy@gmail.com>
2017-05-18 02:33:30 +0530

    FindeOne added to Controller

--- src/main/java/tracksys/model/CompanyController.java
+++ src/main/java/tracksys/model/CompanyController.java
@@ -1,27 +1,63 @@
 package tracksys.model;
 
-import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+/**
+ * A class to test interactions with the MySQL database using the UserDao class.
+ *
+ * @author netgloo
+ */
+@Controller
 @RequestMapping("/company")
 public class CompanyController {
 
-	@Autowired
-	private CompanyRepository companyRepository;
-	
-	@RequestMapping(path="/findAll", method = RequestMethod.GET)
-	public Iterable<Company> findByRepo() throws IOException {
-		return companyRepository.findAll();
-	}
-	
-	@RequestMapping(value = "/get/{companyid}", method = RequestMethod.GET)
-	public Company saveByRepo(@PathVariable String companyid) {
-		return companyRepository.findOne(Integer.valueOf(companyid));
-	}
+	  @RequestMapping(value ="/create",method = RequestMethod.PUT)
+	  
+	  public ResponseEntity<Company>  create(@RequestBody Company company) {
+	    try {
+	    	companyRepository.save(company);
+	    }
+	    catch (Exception ex) {
+	      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	    }
+	    return new ResponseEntity<Company>(company, HttpStatus.OK);
+	  }
+	  
+	  @RequestMapping(value ="/getAll",method = RequestMethod.GET)
+	  public ResponseEntity<List<Company>> getAllCompanies() {
+	    List<Company> companyList = null;
+	    try {
+	    	companyList = companyRepository.findAll();
+	    }
+	    catch (Exception ex) {
+		      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		    }
+		    return new ResponseEntity<List<Company>>(companyList, HttpStatus.OK);
+		  }  
+	  
+	  @RequestMapping(value ="/get/{companyId}",method = RequestMethod.GET)
+	  public ResponseEntity<Company> findCompany(@PathVariable String companyId) {
+	    Company company = null;
+	    try {
+	    	company = companyRepository.findOne(Integer.valueOf(companyId));
+	    }
+	    catch (Exception ex) {
+		      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		    }
+		    return new ResponseEntity<Company>(company, HttpStatus.OK);
+		  } 	  
+	  
+	  @Autowired
+	  private CompanyRepository companyRepository;
 }
