178c5e852487d708a11ee3a131978382881faf87
350401 <pavan.rankireddy@gmail.com>
2017-05-18 03:59:05 +0530

    data Chnaged

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,6 @@ INSERT INTO MODEL (ID, VAL) VALUES (5, 'TEST');
 
 INSERT INTO company (id ,name,address,city,tin,phonenumber) VALUES (1, 'IBM','MIndSPace','HYD',1234,99999);
 
-INSERT INTO group (id ,name,companyid,parent) VALUES (1, 'Raymond',1,0);
-INSERT INTO group (id ,name,companyid,parent) VALUES (2, 'Shuttings',1,1);
+INSERT INTO group_dtl(id ,name,companyid,parentid) VALUES (1, 'Raymond',1,0);
+INSERT INTO group_dtl(id ,name,companyid,parentid) VALUES (2, 'Shuttings',1,1);
 
