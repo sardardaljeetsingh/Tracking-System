c2133cafefebffd30b21f815fffcb23e83ce6b3c
350401 <pavan.rankireddy@gmail.com>
2017-05-18 05:50:49 +0530

    scripts updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,6 @@
-INSERT INTO MODEL (ID, VAL) VALUES (1, 'Pavan');
-INSERT INTO MODEL (ID, VAL) VALUES (2, 'Daljeet');
-INSERT INTO MODEL (ID, VAL) VALUES (3, 'Santosh');
-INSERT INTO MODEL (ID, VAL) VALUES (4, 'Karunakar');
-INSERT INTO MODEL (ID, VAL) VALUES (5, 'TEST');
-
+delete * from company;
 INSERT INTO company (id ,name,address,city,tin,phonenumber) VALUES (1, 'IBM','MIndSPace','HYD',1234,99999);
-
+delete * from group_dtl;
 INSERT INTO group_dtl(id ,name,companyid) VALUES (1, 'Raymond',1);
 INSERT INTO group_dtl(id ,name,companyid,parentid) VALUES (2, 'Shuttings',1,1);
 
