831720e66d6f415c7c6551c012a223758ce0a4bb
350401 <pavan.rankireddy@gmail.com>
2017-05-18 05:57:11 +0530

    sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,4 @@
-delete * from company;
 INSERT INTO company (id ,name,address,city,tin,phonenumber) VALUES (1, 'IBM','MIndSPace','HYD',1234,99999);
-delete * from group_dtl;
 INSERT INTO group_dtl(id ,name,companyid) VALUES (1, 'Raymond',1);
 INSERT INTO group_dtl(id ,name,companyid,parentid) VALUES (2, 'Shuttings',1,1);
 
