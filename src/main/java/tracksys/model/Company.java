1c1abac5e4a286e480b1457c7b7b8be25b52fa53
350401 <pavan.rankireddy@gmail.com>
2017-05-23 21:33:28 +0530

    company pojo changed

--- src/main/java/tracksys/model/Company.java
+++ src/main/java/tracksys/model/Company.java
@@ -1,14 +1,10 @@
 package tracksys.model;
 
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -25,6 +21,11 @@ public class Company {
 	@Size(min = 3, max = 100)
 	@Column(name = "name", nullable = false)
 	private String name;
+	
+	@NotNull
+	@Size(min = 3, max = 100)
+	@Column(name = "name", nullable = false)
+	private String mailingname;	
 
 	@NotNull
 	@Size(min = 2, max = 100)
@@ -32,32 +33,61 @@ public class Company {
 	private String address;
 
 	@NotNull
-	@Size(min = 2, max = 100)
-	@Column(name = "city", nullable = false)
-	private String city;
+	@Column(name = "country")
+	private int country;
+	
+	@NotNull
+	@Column(name = "companystate")
+	private int state;	
 
 	@NotNull
-	@Column(name = "tin", nullable = false)
-	private int tin;
+	@Column(name = "pincode")
+	private int pincode;
 
-	@Column(name = "phonenumber", nullable = false)
-	private int phonenumber;
+	@Column(name = "phone")
+	private int phone;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Size(min = 2, max = 100)
+	@Column(name = "currencesymbol", nullable = true)
+	private String currencesymbol;
+	
+	@NotNull
+	@Column(name = "companytype", nullable = false)
+	private int type;
+	
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	@Column(name = "yearstart", nullable = false)
+	private String yearstart;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	@Column(name = "booksstart", nullable = false)
+	private String booksstart;
+	
 	
 /*	@OneToMany(cascade=CascadeType.ALL, mappedBy="company") 
-	private Set<Group> groups;*/
+	private Set<Group> groups;
+	
+    public Set<Group> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(Set<Group> groups) {
+		this.groups = groups;
+	}
+	*/
 
 	public Company() { }
 
 	public Company(int id) { 
 		this.id = id;
-	}
-
-	public Company(String name, String address, String city,int tin, int phonenumber) {
-		this.name = name;
-		this.address = address;
-		this.city = city;
-		this.tin = tin;
-		this.phonenumber = phonenumber;
 	}
 
 	public int getId() {
@@ -76,6 +106,14 @@ public class Company {
 		this.name = name;
 	}
 
+	public String getMailingname() {
+		return mailingname;
+	}
+
+	public void setMailingname(String mailingname) {
+		this.mailingname = mailingname;
+	}
+
 	public String getAddress() {
 		return address;
 	}
@@ -84,38 +122,77 @@ public class Company {
 		this.address = address;
 	}
 
-	public String getCity() {
-		return city;
+	public int getCountry() {
+		return country;
 	}
 
-	public void setCity(String city) {
-		this.city = city;
+	public void setCountry(int country) {
+		this.country = country;
 	}
 
-	public int getTin() {
-		return tin;
+	public int getState() {
+		return state;
 	}
 
-	public void setTin(int tin) {
-		this.tin = tin;
+	public void setState(int state) {
+		this.state = state;
 	}
 
-	public int getPhonenumber() {
-		return phonenumber;
+	public int getPincode() {
+		return pincode;
 	}
 
-	public void setPhonenumber(int phonenumber) {
-		this.phonenumber = phonenumber;
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
 	}
 
-/*	public Set<Group> getGroups() {
-		return groups;
+	public int getPhone() {
+		return phone;
 	}
 
-	public void setGroups(Set<Group> groups) {
-		this.groups = groups;
-	}*/
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCurrencesymbol() {
+		return currencesymbol;
+	}
+
+	public void setCurrencesymbol(String currencesymbol) {
+		this.currencesymbol = currencesymbol;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getYearstart() {
+		return yearstart;
+	}
+
+	public void setYearstart(String yearstart) {
+		this.yearstart = yearstart;
+	}
+
+	public String getBooksstart() {
+		return booksstart;
+	}
+
+	public void setBooksstart(String booksstart) {
+		this.booksstart = booksstart;
+	}
 
 
 }
