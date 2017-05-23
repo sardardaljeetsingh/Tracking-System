2e7f7ae6af83f9ae4566ffa97c8ccf169e38b534
350401 <pavan.rankireddy@gmail.com>
2017-05-24 02:11:48 +0530

    company object chnaged

--- src/main/java/tracksys/model/Company.java
+++ src/main/java/tracksys/model/Company.java
@@ -1,10 +1,14 @@
 package tracksys.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -72,7 +76,7 @@ public class Company {
 	private String booksstart;
 	
 	
-/*	@OneToMany(cascade=CascadeType.ALL, mappedBy="company") 
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="company") 
 	private Set<Group> groups;
 	
     public Set<Group> getGroups() {
@@ -82,7 +86,7 @@ public class Company {
 	public void setGroups(Set<Group> groups) {
 		this.groups = groups;
 	}
-	*/
+
 
 	public Company() { }
 
