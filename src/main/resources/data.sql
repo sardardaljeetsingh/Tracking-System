c74f3de2b401cd0907c8ab04201ca95cf6312bbd
350401 <pavan.rankireddy@gmail.com>
2017-05-23 21:51:03 +0530

    company insert query chnage

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 
-INSERT INTO company (id ,name,address,city,tin,phonenumber) VALUES (1, 'IBM','IBM','Mind Space',1,1,12345,912345,'pp@pp.com','Rs.',1,'01/04/2017','01/04/2017');
+
+INSERT INTO company (id ,name,mailingname,address,country,companystate,pincode,phone,email,currencesymbol,companytype,yearstart,booksstart) VALUES (1, 'IBM','IBM','Mind Space',1,1,12345,912345,'pp@pp.com','Rs.',1,'01/04/2017','01/04/2017');
 INSERT INTO group_dtl(id ,name,companyid) VALUES (1, 'Raymond',1);
 INSERT INTO group_dtl(id ,name,companyid,parentid) VALUES (2, 'Shuttings',1,1);
 
