71c3392af6d762947c080f820812aaba08b5a24d
350401 <pavan.rankireddy@gmail.com>
2017-06-04 12:25:39 +0530

    service url changed

--- src/main/webapp/js/item-app.js
+++ src/main/webapp/js/item-app.js
@@ -1,6 +1,7 @@
 
 //var hostname ="http://localhost:8080";
-hostname = "http://service-trackingsys.1d35.starter-us-east-1.openshiftapps.com";
+//hostname = "http://service-trackingsys.1d35.starter-us-east-1.openshiftapps.com";
+var hostname = "http://service-itemmngtally.7e14.starter-us-west-2.openshiftapps.com"
 
 
 var app = angular.module("invenApp", ["ngRoute","LocalStorageModule"]);
