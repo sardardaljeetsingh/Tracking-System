43a052b621cc85542fc45f2e22f9bbc2426ae1fc
sardardaljeetsingh <31126688+sardardaljeetsingh@users.noreply.github.com>
2017-09-24 22:30:09 +0530

    Add files via upload

---
+++ src/main/resources/mysql_data.sql
@@ -0,0 +1,9 @@
+
+insert into users (id, address, email, usr_psw, phone, usertype, usr_name,createduser,createddate,modifieduser,modifieddate) values (1, 'HYD', 'admin@admin.com', 'admin', 9999999, 'Admin', 'admin','admin','2017-09-24 21:15:44','admin','2017-09-24 21:15:44');
+
+insert into previliges (id, configuration, accountinfo, inventoryinfo, reports, transactions, userid,createduser,createddate,modifieduser,modifieddate) values (1,'true', 'true', 'true','true', 'true', 1,'admin','2017-09-24 21:15:44','admin','2017-09-24 21:15:44');
+
+INSERT INTO stock_group_dtl(id ,name,companyid,createduser,createddate,modifieduser,modifieddate) VALUES (1, 'DUMMY',1,'admin','2017-09-24 21:15:44','admin','2017-09-24 21:15:44');
+
+INSERT INTO acc_group_dtl(id ,name,companyid,createduser,createddate,modifieduser,modifieddate) VALUES (1, 'DUMMY',1,'admin','2017-09-24 21:15:44','admin','2017-09-24 21:15:44');
+
--- src/main/resources/mysql_schema.sql
+++ src/main/resources/mysql_schema.sql
@@ -1,4 +1,5 @@
----- LOGIN -------
+----13 LOGIN -------
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
@@ -15,24 +16,8 @@ CREATE TABLE users
 	modifieddate datetime NOT NULL
 );
 
-DROP TABLE IF EXISTS previliges;
-CREATE TABLE previliges
-(
-	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	userid INT NOT NULL, 
-	configuration VARCHAR(50) NOT NULL,
-	accountinfo VARCHAR(50) NOT NULL,
-	inventoryinfo VARCHAR(50) NOT NULL,
-	transactions VARCHAR(50) NOT NULL,
-	reports VARCHAR(50) NOT NULL,
-	createduser VARCHAR(50) NOT NULL,
-	createddate datetime NOT NULL,
-	modifieduser VARCHAR(50) NOT NULL,
-	modifieddate datetime NOT NULL
-);
-ALTER TABLE previliges ADD FOREIGN KEY ( userid ) REFERENCES users( id ) ;
 
----- COMPANY -------
+---- 3 COMPANY -------
 
 DROP TABLE IF EXISTS company;
 CREATE TABLE company
@@ -56,6 +41,8 @@ CREATE TABLE company
 	modifieddate datetime NOT NULL
 );
 
+---- 4 COMPANY_USER_PRIVILIGES -------
+
 DROP TABLE IF EXISTS company_user_previliges;
 CREATE TABLE company_user_previliges
 (
@@ -76,7 +63,7 @@ ALTER TABLE company_user_previliges ADD FOREIGN KEY ( companyid ) REFERENCES com
 ALTER TABLE company_user_previliges ADD FOREIGN KEY ( userid ) REFERENCES users( id ) ;
 
 
----- Inventory Info -------
+---- 9 Inventory Info -------
 DROP TABLE IF EXISTS stock_group_dtl;
 CREATE TABLE stock_group_dtl( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, companyid INT NOT NULL, parentid INT,createduser VARCHAR(50) NOT NULL,
 	createddate datetime NOT NULL,
@@ -86,12 +73,17 @@ CREATE TABLE stock_group_dtl( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name
 ALTER TABLE stock_group_dtl ADD FOREIGN KEY ( companyid ) REFERENCES company( id ) ;
 ALTER TABLE stock_group_dtl ADD FOREIGN KEY ( parentid ) REFERENCES stock_group_dtl( id ) ;
 
+------6 Item--------
+
 DROP TABLE IF EXISTS item_dtl;
 CREATE TABLE item_dtl( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, itemHSN VARCHAR(50) NOT NULL,groupid INT NOT NULL,shade VARCHAR(50) NOT NULL,description VARCHAR(50) NOT NULL,uom VARCHAR(50) NOT NULL,initqundty INT,curqundty INT,rate INT,purcrate INT,createduser VARCHAR(50) NOT NULL,
 	createddate datetime NOT NULL,
 	modifieduser VARCHAR(50) NOT NULL,
 	modifieddate datetime NOT NULL);
 ALTER TABLE item_dtl ADD FOREIGN KEY (groupid) REFERENCES stock_group_dtl(id) ;
+
+------7 Item--------
+
 DROP TABLE IF EXISTS item_dtl_trans;
 CREATE TABLE item_dtl_trans( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL,itemid INT,quandity INT,curqundty INT, pices INT,curpices INT,createduser VARCHAR(50) NOT NULL,
 	createddate datetime NOT NULL,
@@ -99,7 +91,7 @@ CREATE TABLE item_dtl_trans( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name 
 	modifieddate datetime NOT NULL);
 ALTER TABLE item_dtl_trans ADD FOREIGN KEY (itemid) REFERENCES item_dtl(id) ;
 
----- Accounting INFO -------
+---- 1 Accounting INFO -------
 DROP TABLE IF EXISTS acc_group_dtl;
 CREATE TABLE acc_group_dtl( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, companyid INT NOT NULL, parentid INT,createduser VARCHAR(50) NOT NULL,
 	createddate datetime NOT NULL,
@@ -109,6 +101,8 @@ CREATE TABLE acc_group_dtl( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name V
 ALTER TABLE acc_group_dtl ADD FOREIGN KEY ( companyid ) REFERENCES company( id ) ;
 ALTER TABLE acc_group_dtl ADD FOREIGN KEY ( parentid ) REFERENCES acc_group_dtl( id ) ;
 
+------8 Item--------
+
 DROP TABLE IF EXISTS ledger;
 CREATE TABLE ledger( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100) NOT NULL,alias VARCHAR(100) NOT NULL, groupid int,mailingname VARCHAR(100) NOT NULL,mailingaddress VARCHAR(500) ,mailingstate VARCHAR(100) ,saletaxno BIGINT,taxpan BIGINT,opbal INT,curbal INT,createduser VARCHAR(50) NOT NULL,
 	createddate datetime NOT NULL,
@@ -117,6 +111,7 @@ CREATE TABLE ledger( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(
 ALTER TABLE ledger ADD FOREIGN KEY ( groupid ) REFERENCES acc_group_dtl( id ) ;
 
 
+----------10 TRANSACTION--------------
 
 DROP TABLE IF EXISTS transactions;
 CREATE TABLE transactions( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,transtype INT,description VARCHAR(50) NOT NULL,fromledger INT,ledgerid INT,quandity INT,rate INT,voucher VARCHAR(50),transdate VARCHAR(50),createduser VARCHAR(50) NOT NULL,
@@ -126,6 +121,8 @@ CREATE TABLE transactions( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,transtyp
 ALTER TABLE transactions ADD FOREIGN KEY (fromledger) REFERENCES ledger(id) ;
 ALTER TABLE transactions ADD FOREIGN KEY (ledgerid) REFERENCES ledger(id) ;
 
+----------11 TRANSACTION--------------
+
 CREATE TABLE transactions_item( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,transid INT, itemid INT,quandity INT,rate INT,purcrate INT,createduser VARCHAR(50) NOT NULL,
 	createddate datetime NOT NULL,
 	modifieduser VARCHAR(50) NOT NULL,
@@ -133,6 +130,7 @@ CREATE TABLE transactions_item( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,tra
 ALTER TABLE transactions_item ADD FOREIGN KEY (transid) REFERENCES transactions(id) ;
 ALTER TABLE transactions_item ADD FOREIGN KEY (itemid) REFERENCES item_dtl(id) ;
 
+----------12 TRANSACTION--------------
 
 CREATE TABLE transactions_item_dtls( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,transitemid INT, itemdtlid INT,quandity INT,createduser VARCHAR(50) NOT NULL,
 	createddate datetime NOT NULL,
@@ -141,19 +139,7 @@ CREATE TABLE transactions_item_dtls( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KE
 ALTER TABLE transactions_item_dtls ADD FOREIGN KEY (transitemid) REFERENCES transactions_item(id) ;
 ALTER TABLE transactions_item_dtls ADD FOREIGN KEY (itemdtlid) REFERENCES item_dtl_trans(id) ;
 
-DROP TABLE IF EXISTS sales;
-CREATE TABLE sales( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, description VARCHAR(50) NOT NULL,itemid INT,quandity INT,rate INT,voucher VARCHAR(50),createduser VARCHAR(50) NOT NULL,
-	createddate datetime NOT NULL,
-	modifieduser VARCHAR(50) NOT NULL,
-	modifieddate datetime NOT NULL);
-ALTER TABLE sales ADD FOREIGN KEY (itemid) REFERENCES item_dtl(id) ;
-
-DROP TABLE IF EXISTS purchages;
-CREATE TABLE purchages( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, description VARCHAR(50) NOT NULL,itemid INT,quandity INT,rate INT,voucher VARCHAR(50),createduser VARCHAR(50) NOT NULL,
-	createddate datetime NOT NULL,
-	modifieduser VARCHAR(50) NOT NULL,
-	modifieddate datetime NOT NULL);
-ALTER TABLE purchages ADD FOREIGN KEY (itemid) REFERENCES item_dtl(id) ;
+------5 Item--------
 
 DROP TABLE IF EXISTS item_current_stock;
 CREATE TABLE item_current_stock( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, itemid INT,quandity INT,createduser VARCHAR(50) NOT NULL,
@@ -163,7 +149,7 @@ CREATE TABLE item_current_stock( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, i
 ALTER TABLE item_current_stock ADD FOREIGN KEY (itemid) REFERENCES item_dtl(id) ;
 
 
----- LOGIN -------
+---- 2 AGENT -------
 DROP TABLE IF EXISTS agents;
 CREATE TABLE agents
 (
@@ -182,6 +168,43 @@ CREATE TABLE agents
 ALTER TABLE agents ADD FOREIGN KEY ( companyid ) REFERENCES company( id ) ;
 
 
+
+---------------Tables not used -----------------------
+
+
+DROP TABLE IF EXISTS previliges;
+CREATE TABLE previliges
+(
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userid INT NOT NULL, 
+	configuration VARCHAR(50) NOT NULL,
+	accountinfo VARCHAR(50) NOT NULL,
+	inventoryinfo VARCHAR(50) NOT NULL,
+	transactions VARCHAR(50) NOT NULL,
+	reports VARCHAR(50) NOT NULL,
+	createduser VARCHAR(50) NOT NULL,
+	createddate datetime NOT NULL,
+	modifieduser VARCHAR(50) NOT NULL,
+	modifieddate datetime NOT NULL
+);
+ALTER TABLE previliges ADD FOREIGN KEY ( userid ) REFERENCES users( id ) ;
+
+DROP TABLE IF EXISTS sales;
+CREATE TABLE sales( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, description VARCHAR(50) NOT NULL,itemid INT,quandity INT,rate INT,voucher VARCHAR(50),createduser VARCHAR(50) NOT NULL,
+	createddate datetime NOT NULL,
+	modifieduser VARCHAR(50) NOT NULL,
+	modifieddate datetime NOT NULL);
+ALTER TABLE sales ADD FOREIGN KEY (itemid) REFERENCES item_dtl(id) ;
+
+DROP TABLE IF EXISTS purchages;
+CREATE TABLE purchages( `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, description VARCHAR(50) NOT NULL,itemid INT,quandity INT,rate INT,voucher VARCHAR(50),createduser VARCHAR(50) NOT NULL,
+	createddate datetime NOT NULL,
+	modifieduser VARCHAR(50) NOT NULL,
+	modifieddate datetime NOT NULL);
+ALTER TABLE purchages ADD FOREIGN KEY (itemid) REFERENCES item_dtl(id) ;
+
+
+-----------------------------------------------------------------------
 
 -----ALTER TABLE FOR EXISTING TABLE STRUCTURE-----------
 alter table agents
@@ -210,6 +233,45 @@ add column modifieduser varchar(50),
 add column modifieddate datetime;
 
 alter table company_user_previliges
+add column createduser varchar(50),
+add column createddate datetime,
+add column modifieduser varchar(50),
+add column modifieddate datetime;
+
+
+alter table item_dtl
+add column createduser varchar(50),
+add column createddate datetime,
+add column modifieduser varchar(50),
+add column modifieddate datetime;
+
+
+alter table item_dtl_trans
+add column createduser varchar(50),
+add column createddate datetime,
+add column modifieduser varchar(50),
+add column modifieddate datetime;
+
+
+alter table stock_group_dtl
+add column createduser varchar(50),
+add column createddate datetime,
+add column modifieduser varchar(50),
+add column modifieddate datetime;
+
+alter table transactions
+add column createduser varchar(50),
+add column createddate datetime,
+add column modifieduser varchar(50),
+add column modifieddate datetime;
+
+alter table transactions_item
+add column createduser varchar(50),
+add column createddate datetime,
+add column modifieduser varchar(50),
+add column modifieddate datetime;
+
+alter table users
 add column createduser varchar(50),
 add column createddate datetime,
 add column modifieduser varchar(50),
